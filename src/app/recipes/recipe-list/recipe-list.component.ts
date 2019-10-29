08e04f9a8bfb686ccc4b98d07e4f5b98efc18a06
System Administrator <root@Sandeeps-MacBook-Air.local>
2019-10-29 15:09:50 +0530

    addinf files to repo

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','A test recipe description','https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_1280,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
